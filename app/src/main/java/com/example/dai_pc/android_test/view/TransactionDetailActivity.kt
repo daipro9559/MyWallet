11f280b5c65f60454d79fd334181dc29a08ea042
dainv <daipro9559@gmail.com>
2018-07-18 17:55:20 +0700

    .

---
+++ app/src/main/java/com/example/dai_pc/android_test/view/TransactionDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dai_pc.android_test.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TransactionDetailActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
